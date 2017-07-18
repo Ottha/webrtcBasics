62fd38d5eabb2ad729fa943a92a071a3a9e2e143
akraf <adrian.kraft@esailors.com>
2017-07-18 18:52:38 +0000

    Add input for remoteid and button to connect style button add connection method add forms module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +6,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  mypeerid: String;
+  myPeerId: String;
+  @Input() someonesId: String;
+  connectedWithSomeone: Boolean = false;
+  connectedPeople: Array<String> = [];
   peer;
 
   ngOnInit() {
     this.peer = new Peer('Ottha', {key: '1335g8zosqtr19k9'});
     this.peer.on('open', (id) => {
-      this.mypeerid = id;
+      this.myPeerId = id;
       console.log('Your connection is now open with  peerId: ' + id);
+    });
+  }
+
+  connect() {
+    const conn = this.peer.connect(this.someonesId);
+    conn.on('open', () => {
+      this.connectedWithSomeone = true;
+      this.connectedPeople.push(this.someonesId);
     });
   }
 }
