ddecf09fd05f5e5ac5de70967004083541ea6d4c
akraf <adrian.kraft@esailors.com>
2017-07-18 18:14:36 +0000

    add peerjs script add typing connect to peerjs server display peerid in template add basic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  mypeerid: String;
+  peer;
+
+  ngOnInit() {
+    this.peer = new Peer('Ottha', {key: '1335g8zosqtr19k9'});
+    this.peer.on('open', (id) => {
+      this.mypeerid = id;
+      console.log('Your connection is now open with  peerId: ' + id);
+    });
+  }
 }
